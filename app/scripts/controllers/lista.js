ef581e2089e2c046ae651fa9c3bdeb6d1e52d0fc
pablo <pablo.santos@ccc.ufcg.edu.br>
2017-05-19 21:27:19 -0300

    adicionado os filtros de ordenação em nome e telefone

--- app/scripts/controllers/lista.js
+++ app/scripts/controllers/lista.js
@@ -14,8 +14,8 @@ angular.module('listaTelefonicaApp')
 
         this.contatos = [
             { nome: "Pedro", telefone: "99767888", cor: "yellow", operadora: { nome: "Oi", codigo: 14, categoria: "Celular" }, data: new Date() },
-            { nome: "Ana", telefone: "99767888", cor: "blue", operadora: { nome: "Vivo", codigo: 15, categoria: "Celular" }, data: new Date() },
-            { nome: "Maria", telefone: "99767888", cor: "red", operadora: { nome: "Tim", codigo: 41, categoria: "Celular" }, data: new Date() }
+            { nome: "Ana", telefone: "99762888", cor: "blue", operadora: { nome: "Vivo", codigo: 15, categoria: "Celular" }, data: new Date() },
+            { nome: "Maria", telefone: "99717888", cor: "red", operadora: { nome: "Tim", codigo: 41, categoria: "Celular" }, data: new Date() }
         ];
 
         this.operadoras = [
@@ -42,6 +42,12 @@ angular.module('listaTelefonicaApp')
             return contatos.some(function(contato) {
                 return contato.selecionado;
             });
+        };
+
+        this.ordenarPor = function(campo) {
+            this.criterioDeOrdenacao = campo;
+            this.direcaoDaOrdenacao = !this.direcaoDaOrdenacao;
+            console.log(this.criterioDeOrdenacao);
         };
 
     });
